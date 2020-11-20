dc6bcabcfcdd49d1277ac765b42479061ffd04f2
ahandnr <68496198+ahandnr@users.noreply.github.com>
2020-11-20 13:09:40 +0700

    Garis besar program utama
    tambah2in buat nanti digabungnya

---
+++ mainprogram.c
@@ -0,0 +1,25 @@
+/*
+TULIS URUTAN FUNGSI DI PROGRAM UTAMA KAYA APA,
+Dikasih komen ya biar paham
+
+CreatePlayer();
+
+// LOAD MAP
+MAP M1,M2,M3,M4;
+LoadMap(M1,M2,M3,M4);
+
+while (true)    // LOOP PROGRAMMNYA
+    TulisMap();                   // Print Map
+    Tulis_Nama_Money_dll();       // Print attribute kaya nama, money, waktu dll
+    Minta_input_perintah(input);  // scanf kata/huruf, misal "buy","w","a","upgrade",dll
+    
+    if ( input = W/A/S/D )        // berarti ingin pindah di map
+        FMap();                   // fungsi dimap, pindah posisi, ubah map, dll
+        cls();                    // clear screen
+        
+    else if (misal input "upgrade", "buy", dll)
+        Contoh();
+    else misal input "upgrade", "buy", dll)
+        Contoh();
+    cls();
+*/
